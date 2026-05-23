462bd0cfcc00406597e0bab42a433282941d3006
D3XXO <29daffa03@gmail.com>
2026-05-24 06:35:19 +0700

    ritem 1 debug

--- Assets/Script/ManagerScript (Gameplay)/RhythmManager.cs
+++ Assets/Script/ManagerScript (Gameplay)/RhythmManager.cs
@@ -48,15 +48,15 @@ public class RhythmManager : MonoBehaviour
             }
             else
             {
-                if (distance < 20f)
+                if (distance <= 30f)
                 {
-                    stateManager.FishingState.ChangeProgress(1.5f);
+                    stateManager.FishingState.ChangeProgress(0.15f);
                 }
-                else if (distance < 50f)
+                else if (distance <= 50f)
                 {
                     stateManager.FishingState.ChangeProgress(0.05f);
                 }
-                else
+                else 
                 {
                     stateManager.FishingState.ChangeProgress(-0.1f);
                     stateManager.TriggerShake(1f, 0.5f);
@@ -80,8 +80,8 @@ public class RhythmManager : MonoBehaviour
         Color color;
 
         if (distance >= 999f) { message = "BAD"; color = Color.red; }
-        else if (distance < 20f) { message = "PERFECT"; color = Color.green; }
-        else if (distance < 50f) { message = "GOOD"; color = Color.yellow; }
+        else if (distance <= 30f) { message = "PERFECT"; color = Color.green; }
+        else if (distance <= 50f) { message = "GOOD"; color = Color.yellow; }
         else { message = "BAD"; color = Color.red; }
 
         GameObject go = Instantiate(feedbackTextPrefab, feedbackPanel);
